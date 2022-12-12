4d44e5bfae0b4eda71a5e5125e9e03c9f565e93c
Matthieu REMOND <matthieu.remond@sfr.fr>
2022-12-12 13:46:23 +0100

    add files

---
+++ includes/functions.php
@@ -0,0 +1,39 @@
+<?php
+
+// Authorization Using the Client Credentials Flow
+
+require 'vendor/autoload.php';
+
+// Hook
+add_action("wp_head", "SpotifySearchBar");
+add_action("wp_head", "SpotifySearch");
+
+function SpotifySearchBar() {
+    echo "
+    <center>
+    <form action=\"\" method=\"GET\">
+    <input name=\"search\" id=\"search\" type=\"text\" placeholder=\"Search on Spotify\">
+    <input id=\"submit\" type=\"submit\" value=\"Search\">
+    </form>
+    </center>
+    ";
+}
+
+function SpotifySearch() {
+    if (!empty($_GET)) {
+        $session = new SpotifyWebAPI\Session(
+            '8d4e36b37abf4965917bc9fd437ab54d',
+            '196a5af2d5c544fd8c612e42190cb86b'
+        );
+        
+        $session->requestCredentialsToken();
+        $accessToken = $session->getAccessToken();
+
+        $api = new SpotifyWebAPI\SpotifyWebAPI();
+        $api->setAccessToken($accessToken);
+
+        echo $_GET["search"];
+        $results = $api->search($_GET["search"], 'album,artist,playlist,track');
+        print_r($results);
+    }
+}
---
+++ plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: MR et EPY Plugin 
+Plugin URI: http://www.google.com/
+Description: Plugin Spotify
+Author: MR et EPY
+Version: 1.0
+Author URI: http://www.google.com/
+*/
+
+// Includes
+require_once plugin_dir_path(__FILE__).'includes/functions.php';
